95938b01494d1e7b77e858e85a0be08938d2fa79
eclipse_user <eclipse123@qq.com>
2020-06-02 22:28:41 +0800

    more edit

---
+++ src/lwd/Test2.java
@@ -0,0 +1,27 @@
+package lwd;
+
+public class Test2 {
+	public String name;
+	public String id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	@Override
+	public String toString() {
+		return "Test2 [name=" + name + ", id=" + id + "]";
+	}
+	public Test2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
